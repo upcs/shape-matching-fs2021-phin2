e322538fe6b153d5981523a51b500bf3780d8086
phin2 <nguyephi24@up.edu>
2021-09-13 12:15:32 -0700

    Added rotate method, and started check method

--- app/src/main/java/edu/up/cs301/shapefitter/MyShapeSolver.java
+++ app/src/main/java/edu/up/cs301/shapefitter/MyShapeSolver.java
@@ -150,7 +150,6 @@ public class MyShapeSolver extends ShapeSolver {
         boolean wellFormed = true;
         int emptySquares = 0;
         int adjSquares = 4;
-
         for (int i = 0; i < shape.length; i++) {
             for (int j = 0; j < shape[i].length; j++) {
                 if (shape[i][j]) {
